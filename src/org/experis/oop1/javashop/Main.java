ea4f12bbf1b38979dd2772ac9314ff310b14bae7
edocico <138378767+edocico@users.noreply.github.com>
2024-03-29 15:20:02 +0100

    java shop part 1

---
+++ src/org/experis/oop1/javashop/Main.java
@@ -0,0 +1,30 @@
+package org.experis.oop1.javashop;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // istanzio nuovo prodotto
+
+        Product console = new Product();
+
+        // chiamo i setter
+
+        console.setName("Playstation");
+
+        console.setDescription("una macchina per giocare ai videogiochi");
+
+        console.setPrice(399.99);
+
+        console.setIva(22);
+
+        // chiamo i getter
+
+        double consolePrice = console.getPrice();
+        int consoleIva = console.getIva();
+
+        double consoleBasePrice = console.getBasePrice(consolePrice, consoleIva);
+
+        System.out.println(consoleBasePrice);
+
+    }
+}
---
+++ src/org/experis/oop1/javashop/Product.java
@@ -0,0 +1,64 @@
+package org.experis.oop1.javashop;
+
+
+import java.util.Random;
+
+public class Product {
+    private int code;
+    private String name;
+    private String description;
+    private double price;
+    private int iva;
+
+    Random random = new Random();
+
+    private int x = random.nextInt(99999999);
+
+    // setter
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+
+    // getter
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getIva() {
+        return iva;
+    }
+
+    // constructor
+
+
+    public Product() {
+        setCode(x);
+
+    }
+
+    // methods
+
+    double getBasePrice(double price, int iva) {
+
+        double productIva = (price * iva) / 100;
+        double basePrice = price - productIva;
+        return basePrice;
+    }
+}
